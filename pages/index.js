7fe5dc4f688de8c6f1203af8c61e6c9e5124cc68
icld <ian@icldesign.com>
2021-11-16 13:51:26 -0800

    Add react-canvas-draw.  Implement in /index

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,49 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import Head from 'next/head';
+import CanvasDraw from 'react-canvas-draw';
 
 export default function Home() {
   const [bgBlack, setBgBlack] = useState(false);
+  const [brushRadius, setBrushRadius] = useState(12);
 
   const handleBgBlack = () => {
     setBgBlack(!bgBlack);
   };
 
+  useEffect(() => {
+    ReactDOM.render(
+      <CanvasDraw
+        hideGrid={true}
+        canvasWidth={500}
+        canvasHeight={500}
+        brushRadius={brushRadius}
+      />,
+      document.querySelector('.main')
+    );
+  }, [brushRadius]);
+
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen py-2'>
+    <div className='flex flex-col min-h-screen py-2 m-auto'>
       <Head>
         <title>Create Next App</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
+      {/* Control Center */}
+      <div className='absolute flex items-center justify-center bg-gray-800 w-52 h-52'>
+        <input
+          className='absolute h-3 overflow-hidden bg-gray-400 rounded-lg appearance-none w-128'
+          type='range'
+          min='1'
+          max='50'
+          step='1'
+          value={brushRadius}
+          onChange={(e) => setBrushRadius(e.target.value)}
+        />
+      </div>
       <div
-        className={`w-full h-screen ${bgBlack ? 'bg-black' : ' bg-blue-900'}`}
+        className={` main    w-full h-screen bg-black flex items-center justify-center`}
         onClick={handleBgBlack}
       />
     </div>
