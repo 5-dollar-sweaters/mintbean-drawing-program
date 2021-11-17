579d6960c6bd64267ccfda591992326fc3c6a821
icld <ian@icldesign.com>
2021-11-16 21:20:38 -0800

    Add routing, some styling

---
+++ components/Landing/Hero.tsx
@@ -0,0 +1,11 @@
+import Nav from './Nav/Nav';
+
+const Hero = () => {
+  return (
+    <div className='w-full h-full'>
+      <Nav />
+    </div>
+  );
+};
+
+export default Hero;
---
+++ pages/index.tsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
+import Head from 'next/head';
+import CanvasDraw from 'react-canvas-draw';
+import { useUser } from '@auth0/nextjs-auth0';
+import Hero from '../components/Landing/Hero';
+
+export default function Home() {
+  const { user } = useUser();
+
+  return (
+    <div className='flex flex-col min-h-screen py-2 m-auto'>
+      <Head>
+        <title>Create Next App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <div className='z-10 flex flex-col w-full text-black'>
+        <Hero />
+      </div>
+    </div>
+  );
+}
