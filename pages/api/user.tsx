8258fa21ea78701a44736c71a091e77d41bb2656
icld <ian@icldesign.com>
2021-11-17 12:34:31 -0800

    Add api routes, swr, and login support

--- pages/api/user.tsx
+++ pages/api/user.tsx
@@ -1,17 +1,18 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
 import prisma from '../../lib/prisma/prisma';
 import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 
-export default withApiAuthRequired(async function user(req, res) {
-  const session = getSession(req, res);
-  console.log(session.user.email);
+export default async function user(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession(req, res);
   const activeUser = await prisma.user.findFirst({
     where: {
-      email: session.user.email,
+      email: session?.user?.email,
     },
     include: {
       drawings: true,
     },
   });
-  console.log(activeUser);
+
   res.json(activeUser);
-});
+}
--- pages/draw.tsx
+++ pages/draw.tsx
@@ -1,19 +1,37 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import {
+  GetServerSideProps,
+  NextPage,
+  InferGetServerSidePropsType,
+} from 'next';
 import Head from 'next/head';
 import CanvasDraw from 'react-canvas-draw';
-import { useUser } from '@auth0/nextjs-auth0';
+import { useUser, getSession } from '@auth0/nextjs-auth0';
+import useSWR from 'swr';
+import { fetcher } from '../lib/swr/fetcher';
+import prisma from '../lib/prisma/prisma';
 
-export default function Draw() {
+const Draw: NextPage = (
+  props: InferGetServerSidePropsType<typeof getServerSideProps>
+) => {
   const [brushRadius, setBrushRadius] = useState(12);
   const [canvasWidth, setCanvasWidth] = useState(500);
   const [canvasHeight, setCanvasHeight] = useState(500);
+  const canvasRef: any = useRef();
+  const { user } = useUser();
+  const { data, error, mutate } = useSWR('/api/user', fetcher);
+  console.log(data);
+  console.log(user);
 
-  const { user, loading } = useUser();
+  console.log(canvasRef.current);
+
+  type DATA_TO_SAVE = { sketchId: string; userId: string };
 
   useEffect(() => {
     ReactDOM.render(
       <CanvasDraw
+        ref={canvasRef}
         hideGrid={true}
         canvasWidth={canvasWidth}
         canvasHeight={canvasHeight}
@@ -24,6 +42,19 @@ export default function Draw() {
     );
   }, [brushRadius, canvasWidth, canvasHeight]);
 
+  const handleSave = async () => {
+    const savedDrawing = canvasRef?.current?.getSaveData();
+    const dataToSave: DATA_TO_SAVE = {
+      sketchId: savedDrawing,
+      userId: 'fornow',
+    };
+    try {
+      // await saveData(dataToSave);
+      console.log(dataToSave);
+      // console.log('You saved it!');
+    } catch (error) {}
+  };
+
   return (
     <div className='flex flex-col min-h-screen py-2 m-auto'>
       <Head>
@@ -32,7 +63,16 @@ export default function Draw() {
       </Head>
 
       <div className='z-10 flex flex-col w-full text-black'></div>
-
+      <button
+        className='absolute z-30'
+        onClick={() => {
+          const saveIT = canvasRef?.current?.getSaveData();
+          console.log(saveIT);
+          return saveIT;
+        }}
+      >
+        saveIt
+      </button>
       {/* Control Center */}
       <div className='absolute z-10 flex flex-col items-center bg-gray-800 justify-evenly w-52 h-52'>
         {/* Brush Stroke */}
@@ -67,7 +107,7 @@ export default function Draw() {
         />
       </div>
 
-      {/* Canvas div  className='main' */}
+      {/* Canvas   */}
       <div className='flex items-center justify-center h-screen bg-gray-400'>
         <div
           className={` main    bg-gray-400 flex items-center justify-center  rounded-3xl  overflow-hidden`}
@@ -75,4 +115,18 @@ export default function Draw() {
       </div>
     </div>
   );
-}
+};
+
+export default Draw;
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const userReq: any = await fetch(`${process.env.NEXT_APP_URL}/api/user`);
+  const dbUser: any = await userReq.json();
+  // const drawingReq: any = await fetch(``);
+  console.log(dbUser);
+  return {
+    props: {
+      dbUser,
+    },
+  };
+};
