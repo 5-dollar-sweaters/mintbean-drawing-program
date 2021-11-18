5e12f520d6f2f517c425d0cf16c39e5cdf342992
icld <ian@icldesign.com>
2021-11-18 15:33:10 -0800

    Fix compiling error

---
+++ pages/api/drawings.js
@@ -0,0 +1,17 @@
+// import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../lib/prisma/prisma';
+
+export default async function drawings(req, res) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method not allowed ' });
+  }
+
+  const query = await req.query.ownerId;
+
+  const drawings = await prisma.drawing.findMany({
+    where: {
+      ownerId: query,
+    },
+  });
+  res.json(drawings);
+}
