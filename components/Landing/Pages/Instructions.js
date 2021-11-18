36d558e1f4a1c2801626330c4888e88913467fe5
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-11-18 15:56:01 -0800

    add instructions text

--- components/Landing/Pages/Instructions.js
+++ components/Landing/Pages/Instructions.js
@@ -2,24 +2,105 @@ const Instructions = () => {
   return (
     <div id="container" className=" bg-purple-400 h-screen">
       <div id="top">
-        <div id="title" className=" text-7xl text-white font-fancy">
+        <div
+          id="title"
+          className=" flex justify-center text-6xl text-white font-fancy pt-16"
+        >
           <h1>A helping hand</h1>
         </div>
-        <div id="description" className="  text-4xl text-white">
+        <div
+          id="description"
+          className=" flex justify-center text-center px-16 py-4 text-2xl text-white"
+        >
           <h2>
-            Struggling to get started? Scroll down to see a list of option plus
-            some handy tips and tricks
+            Struggling to get started? <br></br>Scroll down to see a list of
+            option plus some handy tips and tricks
           </h2>
         </div>
       </div>
       <div id="bottom">
         <div id="image-row">
-          <div id="cards">
-            <div id="card" className=" h-16 w-10 bg-purple-400 border-4"></div>
-            <div id="card" className=" h-16 w-10 bg-purple-400 border-4"></div>
-            <div id="card" className=" h-16 w-10 bg-purple-400 border-4"></div>
-            <div id="card" className=" h-16 w-10 bg-purple-400 border-4"></div>
-            <div id="card" className=" h-16 w-10 bg-purple-400 border-4"></div>
+          <div
+            id="cards"
+            className=" flex flex-row  bg-gray-200 w-screen px-8 py-4 border-"
+          >
+            <div
+              id="card"
+              className="  bg-purple-400 w-64 h-72 border-4 p-4 border-black rounded-2xl "
+            >
+              <div
+                id="card-text"
+                className=" flex text-center text-white text-2xl"
+              >
+                Start by clicking the arrows on the sides of the canvas
+              </div>
+            </div>
+            <div
+              id="card"
+              className="  bg-purple-400 w-64 h-72 border-4 py-4 border-black rounded-2xl "
+            >
+              <div
+                id="card-text"
+                className=" flex text-center text-white text-2xl"
+              >
+                Start by clicking the arrows on the sides of the canvas
+              </div>
+            </div>
+            <div
+              id="card"
+              className="  bg-purple-400 w-64 h-72 border-4 py-4 border-black rounded-2xl "
+            >
+              <div
+                id="card-text"
+                className=" flex text-center text-white text-2xl"
+              >
+                Start by clicking the arrows on the sides of the canvas
+              </div>
+            </div>
+            <div
+              id="card"
+              className="  bg-purple-400 w-64 h-72 border-4 py-4 border-black rounded-2xl "
+            >
+              <div
+                id="card-text"
+                className=" flex text-center text-white text-2xl"
+              >
+                Start by clicking the arrows on the sides of the canvas
+              </div>
+            </div>
+            <div
+              id="card"
+              className="  bg-purple-400 w-64 h-72 border-4 py-4 border-black rounded-2xl "
+            >
+              <div
+                id="card-text"
+                className=" flex text-center text-white text-2xl"
+              >
+                Start by clicking the arrows on the sides of the canvas
+              </div>
+            </div>
+            <div
+              id="card"
+              className="  bg-purple-400 w-64 h-72 border-4 py-4 border-black rounded-2xl "
+            >
+              <div
+                id="card-text"
+                className=" flex text-center text-white text-2xl"
+              >
+                Start by clicking the arrows on the sides of the canvas
+              </div>
+            </div>
+            <div
+              id="card"
+              className="  bg-purple-400 w-64 h-72 border-4 py-4 border-black rounded-2xl "
+            >
+              <div
+                id="card-text"
+                className=" flex text-center text-white text-2xl"
+              >
+                Start by clicking the arrows on the sides of the canvas
+              </div>
+            </div>
           </div>
         </div>
       </div>
