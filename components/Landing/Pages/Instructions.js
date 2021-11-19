3708e5547ffff22cbd8d36e9ad994be1f997defa
icld <ian@icldesign.com>
2021-11-19 14:09:14 -0800

    Add Zoom?

--- components/Landing/Pages/Instructions.js
+++ components/Landing/Pages/Instructions.js
@@ -1,18 +1,18 @@
-import { instructions } from "lib/instructionData/instructionData";
+import { instructions } from 'lib/instructionData/instructionData';
 
 const Instructions = () => {
   return (
-    <div id="container" className=" bg-gray-900 h-screen mt-8 lg:mt-0">
-      <div id="top">
+    <div id='container' className='h-screen mt-8 bg-gray-900 lg:mt-0'>
+      <div id='top'>
         <div
-          id="title"
-          className=" flex justify-center text-4xl lg:text-6xl text-white font-fancy pt-16"
+          id='title'
+          className='flex justify-center pt-16 text-4xl text-white lg:text-6xl font-fancy'
         >
           <h1>A helping hand</h1>
         </div>
         <div
-          id="description"
-          className=" flex justify-center text-center px-16 py-4 lg:text-2xl text-white"
+          id='description'
+          className='flex justify-center px-16 py-4 text-center text-white lg:text-2xl'
         >
           <h2>
             Struggling to get started? <br></br>Scroll down to see a list of
@@ -20,18 +20,18 @@ const Instructions = () => {
           </h2>
         </div>
       </div>
-      <div id="bottom">
-        <div id="image-row" className="contents">
+      <div id='bottom'>
+        <div id='image-row' className='contents'>
           <div
-            id="cards"
-            className=" w-max border inline-grid grid-cols-7 gap-x-12"
+            id='cards'
+            className='inline-grid grid-cols-7 border w-max gap-x-12'
           >
             {instructions.map((instruction, i) => {
               return (
                 <div
                   key={i}
-                  id="card"
-                  className=" text-white font-fancy bg-purple-400 w-32 lg:w-60 h-64 border-4 p-4 border-black rounded-md "
+                  id='card'
+                  className='w-32 h-64 p-4 text-white bg-purple-400 border-4 border-black rounded-md font-fancy lg:w-60'
                 >
                   {instruction.inst}
                 </div>
