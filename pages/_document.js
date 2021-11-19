8a2ceb6377fbdc2ed0e6ff0a67910ac93ea9c9cc
icld <ian@icldesign.com>
2021-11-19 14:08:59 -0800

    Add overflow X hidden for swiper overage

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -8,20 +8,20 @@ class MyDocument extends Document {
 
   render() {
     return (
-      <Html>
+      <Html className='relative overflow-x-hidden '>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
           <link
-            href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Open+Sans&display=swap"
-            rel="stylesheet"
+            href='https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Open+Sans&display=swap'
+            rel='stylesheet'
           />
         </Head>
 
         {/* TW screen debugger in dev  */}
         <body
-          className={` ${
-            process.env.NODE_ENV !== "production" && "debug-screens"
+          className={` relative overflow-x-hidden ${
+            process.env.NODE_ENV !== 'production' && 'debug-screens'
           }`}
         >
           <Main />
