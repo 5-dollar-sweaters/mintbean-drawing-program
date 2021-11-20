38ee9c45784895d1841e6961c65785374f71f709
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-11-19 16:24:07 -0800

    css for whole landing page

--- components/Landing/Pages/AboutUs.js
+++ components/Landing/Pages/AboutUs.js
@@ -2,62 +2,66 @@ import Image from "next/image";
 
 const AboutUs = () => {
   return (
-    <div
-      id="container"
-      className=" lg:flex-1 bg-gray-400 h-screen text-white text-xs"
-    >
+    <>
+      <a name="About"></a>
       <div
-        id="box"
-        className="flex flex-col content-center items-center justify-center px-32 py-8 lg:pl-24 lg:pt-16"
+        id="container"
+        className=" lg:flex-1 bg-gray-400 h-screen text-white text-xs"
       >
-        <div id="ian" className="  lg:flex ">
-          <div className=" lg:m-4">
-            <Image
-              className=" rounded-full"
-              src="/ian.png"
-              alt="ian"
-              width={200}
-              height={200}
-            />
-          </div>
-          <div id="info" className="lg:w-100 lg:p-16 lg:pl-24">
-            <div id="name" className="py-4 lg:flex lg:flex-row">
-              <p>Ian Cameron Lyles</p>
-              <p className="lg:pl-36">SOFTWARE DEVELOPER</p>
+        <div
+          id="box"
+          className="flex flex-col content-center items-center justify-center px-32 py-8 lg:pl-24 lg:pt-14 "
+        >
+          <div id="ian" className="  lg:flex border-t-2  ">
+            <div className=" lg:m-4">
+              <Image
+                className=" rounded-full"
+                src="/ian.png"
+                alt="ian"
+                width={200}
+                height={200}
+              />
             </div>
-            <div id="ian-description" className="lg:pt-8 text-sm">
-              <p>
-                14 years of experience with all types of things and I know just
-                about all there is to know so dont teach me anything.
-              </p>
+            <div id="info" className="lg:w-100 lg:p-4 lg:pl-24">
+              <div id="name" className="py-4 lg:flex lg:flex-row">
+                <p>Ian Cameron Lyles</p>
+                <p className="lg:pl-36">SOFTWARE DEVELOPER</p>
+              </div>
+              <div id="ian-description" className="lg:pt-8 text-sm">
+                <p>
+                  14 years of experience with all types of things and I know
+                  just about all there is to know so dont teach me anything.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div id="ethan" className="lg:flex">
-          <div className=" lg:m-4">
-            <Image
-              className=" rounded-full"
-              src="/ethan.jfif"
-              alt="ethan"
-              width={200}
-              height={200}
-            />
-          </div>
-          <div id="info" className="lg:w-100 lg:p-16 lg:pl-24">
-            <div id="name" className=" py-4 lg:flex lg:flex-row">
-              <p>Ethan William Pierce</p>
-              <p className="lg:pl-36">SOFTWARE DEVELOPER</p>
+          <div id="ethan" className="lg:flex border-b-2">
+            <div className=" lg:m-4">
+              <Image
+                className=" rounded-full"
+                src="/ethan.jfif"
+                alt="ethan"
+                width={220}
+                height={220}
+              />
             </div>
-            <div id="ethan-description" className="lg:pt-8 text-sm">
-              <p>
-                Listen here you young whippersnapper. This is all the things I
-                know how to do, nothing more. All I know is that I know nothing.
-              </p>
+            <div id="info" className="lg:w-100 lg:p-4 lg:pl-24 ">
+              <div id="name" className=" py-4 lg:flex lg:flex-row ">
+                <p>Ethan William Pierce</p>
+                <p className="lg:pl-36">SOFTWARE DEVELOPER</p>
+              </div>
+              <div id="ethan-description" className="lg:pt-8 text-sm">
+                <p>
+                  Listen here you young whippersnapper. This is all the things I
+                  know how to do, nothing more. All I know is that I know
+                  nothing.
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- components/Landing/Pages/Instructions.js
+++ components/Landing/Pages/Instructions.js
@@ -2,45 +2,48 @@ import { instructions } from "lib/instructionData/instructionData";
 
 const Instructions = () => {
   return (
-    <div id="container" className=" bg-gray-900 h-screen mt-8 lg:mt-0">
-      <div id="top">
-        <div
-          id="title"
-          className=" flex justify-center text-4xl lg:text-6xl text-white font-fancy pt-16"
-        >
-          <h1>A helping hand</h1>
-        </div>
-        <div
-          id="description"
-          className=" flex justify-center text-center px-16 py-4 lg:text-2xl text-white"
-        >
-          <h2>
-            Struggling to get started? <br></br>Scroll down to see a list of
-            option plus some handy tips and tricks
-          </h2>
-        </div>
-      </div>
-      <div id="bottom">
-        <div id="image-row" className="contents">
+    <>
+      <a name="Instructions"></a>
+      <div id="container" className=" bg-gray-900 h-screen mt-8 lg:mt-0">
+        <div id="top">
           <div
-            id="cards"
-            className=" w-max border inline-grid grid-cols-7 gap-x-12"
+            id="title"
+            className=" flex justify-center text-4xl lg:text-6xl text-white font-fancy pt-16"
           >
-            {instructions.map((instruction, i) => {
-              return (
-                <div
-                  key={i}
-                  id="card"
-                  className=" text-white font-fancy bg-purple-400 w-32 lg:w-60 h-64 border-4 p-4 border-black rounded-md "
-                >
-                  {instruction.inst}
-                </div>
-              );
-            })}
+            <h1>A helping hand</h1>
+          </div>
+          <div
+            id="description"
+            className=" flex justify-center text-center px-16 py-4 lg:text-2xl text-white"
+          >
+            <h2>
+              Struggling to get started? <br></br>Scroll down to see a list of
+              option plus some handy tips and tricks
+            </h2>
+          </div>
+        </div>
+        <div id="bottom">
+          <div id="image-row" className="contents">
+            <div
+              id="cards"
+              className=" w-max border inline-grid grid-cols-7 gap-x-12"
+            >
+              {instructions.map((instruction, i) => {
+                return (
+                  <div
+                    key={i}
+                    id="card"
+                    className=" text-white font-fancy bg-purple-400 w-32 lg:w-60 h-64 border-4 p-4 border-black rounded-md "
+                  >
+                    {instruction.inst}
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
