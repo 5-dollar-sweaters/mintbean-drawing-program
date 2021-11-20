005946af4fd214913d7017a0286a48c18195a593
icld <ian@icldesign.com>
2021-11-20 09:02:48 -0800

    Absolute path for fetch

--- components/Draw/Sections/ControlCenter/CanvasDataZone.js
+++ components/Draw/Sections/ControlCenter/CanvasDataZone.js
@@ -9,25 +9,25 @@ const CanvasDataZone = () => {
   const { user } = useUser();
   const { canvasRef, activeUser } = useStore();
   const { showSaveBox, setShowSaveBox } = useSaveStore();
-  const [title, setTitle] = useState('');
+  // const [title, setTitle] = useState('');
 
-  console.log(title);
+  // console.log(title);
 
-  const handleSaveDrawing = async () => {
-    const drawingString = await canvasRef.current.getSaveData();
-    const data = {
-      data: drawingString,
-      ownerId: activeUser?.id,
-      title: title,
-    };
-    try {
-      await saveData(data);
-      await alert('saved');
-      canvasRef.current.clear();
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const handleSaveDrawing = async () => {
+  //   const drawingString = await canvasRef.current.getSaveData();
+  //   const data = {
+  //     data: drawingString,
+  //     ownerId: activeUser?.id,
+  //     title: title,
+  //   };
+  //   try {
+  //     await saveData(data);
+  //     await alert('saved');
+  //     canvasRef.current.clear();
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   const buttonStyle =
     'w-11/12 py-1 text-gray-100 transition-all duration-300   bg-gray-400 rounded-md hover:bg-gray-200 hover:text-gray-600';
