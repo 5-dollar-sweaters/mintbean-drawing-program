83edb4434ca1741e14fb10eb015e8ca473871f06
icld <ian@icldesign.com>
2021-11-20 12:07:47 -0800

    Adress saveDrawing type errors

--- pages/api/saveDrawing.tsx
+++ pages/api/saveDrawing.tsx
@@ -1,6 +1,10 @@
+import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from 'lib/prisma/prisma';
 
-export default async function saveDrawing(req, res) {
+export default async function saveDrawing(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method not allowed ' });
   }
