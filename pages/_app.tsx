1bf51dc473044e8dc7e8b4d2283da4c5f68bce69
icld <ian@icldesign.com>
2021-11-20 16:11:18 -0800

    Add footer component to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import 'tailwindcss/tailwind.css';
 import { UserProvider } from '@auth0/nextjs-auth0';
 import type { AppProps /*, AppContext */ } from 'next/app';
 import Nav from 'components/Landing/Nav/Nav';
+import Footer from 'components/Landing/Pages/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <UserProvider>
       <Nav />
       <Component {...pageProps} />
+      <Footer />
     </UserProvider>
   );
 }
