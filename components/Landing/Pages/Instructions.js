63f973950d05f7d666daff7238bf5731e1685156
Ethan Pierce <ethanpiercepresents@gmail.com>
2021-11-20 23:07:57 -0800

    add scroll trigger and drag cards

--- components/Landing/Pages/Instructions.js
+++ components/Landing/Pages/Instructions.js
@@ -1,8 +1,30 @@
-import { instructions } from 'lib/instructionData/instructionData';
+import { useEffect, useRef } from "react";
+import { scrollingCards } from "./animation";
+import { gsap } from "gsap/dist/gsap";
+import { Draggable } from "gsap/dist/Draggable";
+import { instructions } from "lib/instructionData/instructionData";
+
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(Draggable);
+}
 
 const Instructions = () => {
-  return (
+  let cards = useRef(null);
 
+  useEffect(() => {
+    scrollingCards(cards);
+  }, []);
+
+  useEffect(() => {
+    Draggable.create(cards, {
+      type: "x",
+      edgeResistance: 0.65,
+      bounds: "#container",
+      inertia: true,
+    });
+  }, []);
+
+  return (
     <>
       <a name="Instructions"></a>
       <div id="container" className=" bg-gray-900 h-screen mt-8 lg:mt-0">
@@ -27,14 +49,15 @@ const Instructions = () => {
           <div id="image-row" className="contents">
             <div
               id="cards"
-              className=" w-max border inline-grid grid-cols-7 gap-x-12"
+              ref={(el) => (cards = el)}
+              className=" w-max  inline-grid grid-cols-7 gap-x-12 px-16"
             >
               {instructions.map((instruction, i) => {
                 return (
                   <div
                     key={i}
                     id="card"
-                    className=" text-white font-fancy from-purple-300 bg-gradient-to-l w-32 lg:w-60 h-64 border-4 p-4 border-black rounded-md "
+                    className=" flex justify-center text-center text-black bg-gray-100  h-50 w-32 lg:w-48 lg:h-64 border-4 p-4 border-black rounded-md drop-shadow-sm shadow-lg "
                   >
                     {instruction.inst}
                   </div>
