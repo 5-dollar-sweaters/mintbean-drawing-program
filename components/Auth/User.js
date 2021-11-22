92481668aacc7de7e8f66d4a89a8d9f9f4699b93
icld <ian@icldesign.com>
2021-11-21 18:47:28 -0800

    Mobile Nav has user

--- components/Auth/User.js
+++ components/Auth/User.js
@@ -8,54 +8,47 @@ const User = ({ userFromNav }) => {
   const { activeUser, setActiveUser } = useStore();
   const router = useRouter();
 
+  console.log(name);
   const handleLogOut = async () => {
     try {
       await router.push('/api/auth/logout');
-      await setActiveUser([]);
+      await setActiveUser({});
       alert('logged out');
     } catch (error) {}
   };
 
   return (
-    <div className='w-full'>
-      <div className='flex items-center w-full h-12 '>
-        <div
-          className='flex flex-row duration-200 align-center'
-          // onClick={() => router.push(`/user/${id}`)}
-        >
-          <div className='h-9 w-9 '>
-            <Image
-              src={image || '/rainbow.png'}
-              alt='user profile image'
-              width={36}
-              height={36}
-              className='rounded-full'
-            />
-          </div>
-
-          <div className='ml-2 '>
-            <div className='-mb-2 text-sm font-medium text-gray-700 capitalize '>
-              {name}
-            </div>
-            <button
-              className='text-xs text-gray-400 transition-all duration-150 hover:scale-125 hover:translate-x-1'
-              onClick={() => handleLogOut()}
-            >
-              Logout
-            </button>
-            {/* <p className='h-4 text-xs text-gray-500'>{userName}</p> */}
-          </div>
+    <div className='flex items-center w-full h-12'>
+      <div
+        className='flex flex-row duration-200'
+        // onClick={() => router.push(`/user/${id}`)}
+      >
+        <div className='h-9 w-9 '>
+          <Image
+            src={image || '/rainbow.png'}
+            alt='user profile image'
+            width={36}
+            height={36}
+            className='rounded-full'
+          />
         </div>
 
-        {/* Follow button  - dynamic based on following state
-      {/* <button
-          className={`w-16  p-0.5 shadow-sm text-sm font-medium border rounded-xl ${
-            following ? 'text-white bg-gray-600' : 'text-gray-600 bg-white'
-          } `}
-          onClick={following ? () => handleUnfollow() : () => handleFollow()}
-        >
-          {following ? 'Unfollow' : 'Follow'}
-        </button> */}
+        <div className='z-20 ml-2 '>
+          <div
+            className={`-mb-2 text-sm font-medium  capitalize  ${
+              router.pathname === '/draw' ? 'text-white' : 'text-gray-700'
+            } `}
+          >
+            {name}
+          </div>
+          <button
+            className='text-xs text-gray-400 transition-all duration-150 hover:scale-125 hover:translate-x-1'
+            onClick={() => handleLogOut()}
+          >
+            Logout
+          </button>
+          {/* <p className='h-4 text-xs text-gray-500'>{userName}</p> */}
+        </div>
       </div>
     </div>
   );
