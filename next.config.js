2ff4cf68398f1335caafc852f36645d4dd83613f
icld <ian@icldesign.com>
2021-11-23 14:58:03 -0800

    Confetti add

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
  * @type {import('next').NextConfig}
  **/
 const nextConfig = {
+  experimental: {
+    urlImports: ['https://cdn.skypack.dev'],
+  },
   env: {
     AUTH0_HOOK_SECRET: process.env.AUTH0_HOOK_SECRET,
     NEXT_APP_URL: process.env.NEXT_APP_URL,
